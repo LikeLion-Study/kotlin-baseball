4956ebb401ad01ceecd5873e72502ac8a94c3ee9
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-04-09 16:49:48 +0900

    feat: main 구성 및 BaseballPresenter 의존성 주입

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,12 @@
 package baseball
 
+import baseball.presenter.BaseballPresenter
+import baseball.view.InputViewImpl
+import baseball.view.OutputViewImpl
+
 fun main() {
-    // TODO: 기능 구현
+    val inputView: BaseballContract.InputView = InputViewImpl()
+    val outputView: BaseballContract.OutputView = OutputViewImpl()
+    val baseball = BaseballPresenter(inputView, outputView)
+    baseball.play()
 }
