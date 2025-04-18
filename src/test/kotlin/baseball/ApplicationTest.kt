ec3b685543e4babbf51e2e229e6eeccdc7490a1a
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-04-18 17:07:58 +0900

    Update ApplicationTest.kt

--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -9,38 +9,6 @@ import java.io.PrintStream
 import java.nio.charset.StandardCharsets
 
 class ApplicationTest {
-    private val originalOut = System.out
-    private lateinit var outputStream: ByteArrayOutputStream
-
-    @BeforeEach
-    fun setUp() {
-        outputStream = ByteArrayOutputStream()
-        System.setOut(PrintStream(outputStream, true, StandardCharsets.UTF_8.name()))
-    }
-
-    @AfterEach
-    fun tearDown() {
-        System.setOut(originalOut)
-    }
-
-    @Test
-    fun `게임 시작 및 초기 프롬프트 출력 테스트`() {
-        main()
-
-        val output = outputStream.toString(StandardCharsets.UTF_8.name())
-        assertThat(output).contains("숫자 야구 게임을 시작합니다.")
-        assertThat(output).contains("숫자를 입력해주세요")
-    }
-
-    @Test
-    fun `승리 후 메시지 출력 테스트`() {
-        main()
-
-        val output = outputStream.toString(StandardCharsets.UTF_8.name())
-        assertThat(output).contains("3스트라이크")
-        assertThat(output).contains("3개의 숫자를 모두 맞히셨습니다!")
-        assertThat(output).contains("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-    }
 
     // TODO: 클래스명과 함수명 변경 가능 (RandomNumberGenerator.generate())
     @Test
